0f489ce49824b97910fc58600e78ecd5c804da99
Shubham Ramesh Gawande <shubhamrg866@gmail.com>
2025-02-01 04:18:35 +0530

    Add files via upload

--- myportfolio/portfolio/urls.py
+++ myportfolio/portfolio/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  # Home page
+    path('contact/submit/', views.contact_submit, name='contact_submit'),  # Contact form submission
 ]
